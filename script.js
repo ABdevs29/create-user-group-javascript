6e335b63799607b7e734e3e8cb886d0eff0e3898
Abhishek Sateesh Gaonkar <85872303+ABdevs29@users.noreply.github.com>
2021-12-23 17:15:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Function for carousal scroll in group cards
-// let scrollPosition = 0;
 let cards = document.getElementsByClassName('groups-card');
 
 function slide(direction){
@@ -18,22 +17,6 @@ function slide(direction){
             window.clearInterval(slideVar);
         }
     }, 50);
-
-    // console.log(cards);
-    // if (scrollPosition >= 0 && scrollPosition < cards.length ) {
-    //     if (direction== 'left' && scrollPosition != 0) {
-    //         cards[scrollPosition-1].scrollIntoView(false);
-    //         scrollPosition = scrollPosition-1;
-    //     } else if (direction== 'right' && scrollPosition != cards.length - 1){
-    //         cards[scrollPosition+1].scrollIntoView(false);
-    //         scrollPosition = scrollPosition+1;
-    //     }
-    // }
-
-
-    // cards[1].scrollIntoView(false);
-
-    
 }
 
 
@@ -192,7 +175,7 @@ function sortingDate () {
 
 }
 
-// let counter = 0;
+
 //search function 
 function searchFn () {
     for (let i=0; i< groupName.length; i++) {
@@ -226,67 +209,3 @@ function debounce (fn, d) {
 
 const debounceForSearch = debounce(searchFn, 300);
 
-
-
-
-
-
-
-
-
-
-//Function for sorting tables
-// function sortTable(n) {
-//     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-//     table = document.getElementById("groups-bottom-portion");
-//     switching = true;
-//     //Set the sorting direction to ascending:
-//     dir = "asc"; 
-//     /*Make a loop that will continue until
-//     no switching has been done:*/
-//     while (switching) {
-//       //start by saying: no switching is done:
-//       switching = false;
-//       rows = table.rows;
-//       /*Loop through all table rows (except the
-//       first, which contains table headers):*/
-//       for (i = 1; i < (rows.length - 1); i++) {
-//         //start by saying there should be no switching:
-//         shouldSwitch = false;
-//         /*Get the two elements you want to compare,
-//         one from current row and one from the next:*/
-//         x = rows[i].getElementsByTagName("TD")[n];
-//         y = rows[i + 1].getElementsByTagName("TD")[n];
-//         /*check if the two rows should switch place,
-//         based on the direction, asc or desc:*/
-//         if (dir == "asc") {
-//           if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-//             //if so, mark as a switch and break the loop:
-//             shouldSwitch= true;
-//             break;
-//           }
-//         } else if (dir == "desc") {
-//           if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-//             //if so, mark as a switch and break the loop:
-//             shouldSwitch = true;
-//             break;
-//           }
-//         }
-//       }
-//       if (shouldSwitch) {
-//         /*If a switch has been marked, make the switch
-//         and mark that a switch has been done:*/
-//         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//         switching = true;
-//         //Each time a switch is done, increase this count by 1:
-//         switchcount ++;      
-//       } else {
-//         /*If no switching has been done AND the direction is "asc",
-//         set the direction to "desc" and run the while loop again.*/
-//         if (switchcount == 0 && dir == "asc") {
-//           dir = "desc";
-//           switching = true;
-//         }
-//       }
-//     }
-//   }
